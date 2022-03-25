165ff0dc8942cdabba36948f89f2ff2fdc4605cd
eri-Q <eri.hatanaka8@gmail.com>
2022-03-25 15:36:05 +0000

    画像投稿

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fff7c1054cd2133e17accb64c4fff3ebde8223f6ab621a12eb29b0ae362c1f33c6ab53e80fe479929a0a91ac4fb86aac34d260fb44f9e8a308c3737868846409'
